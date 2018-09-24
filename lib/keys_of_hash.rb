5e102c15e4e36878f1dafae50100148d7ff03fa2
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-09-24 21:39:43 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,9 +1,23 @@
 require 'pry'
 
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     array = []
+    arguments.each do |key, value|
+      binding.pry
+    end
     array
   end
 
 end
+
+#def animals
+#  animals = {
+#    "sugar glider"=>"Australia",
+#    "aye-aye"=> "Madagascar",
+#    "red-footed tortoise"=>"Panama",
+#    "kangaroo"=> "Australia",
+#    "tomato frog"=>"Madagascar",
+#    "koala"=>"Australia"
+#  }
+#end
